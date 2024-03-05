7599a3b65b0b60f8552532952f1e693d1f5a01a9
Jan <147418240+JanEspana@users.noreply.github.com>
2024-03-05 09:59:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP_Ex2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Card card1 = new Card("Red", 5);
+            Console.WriteLine("Card\nColor: {0}. Number: {1}", card1.GetColor(), card1.GetNumber());
+
+            Lamp lamp1 = new Lamp(true, "Green", 18000);
+            Console.WriteLine("Lamp\nIs it on?: {0}. Color: {1}. Electric power: {2}kW", lamp1.GetIsOn(), lamp1.GetColor(), lamp1.GetPower());
+        }
+    }
+}
